f1d6d2362518d4cf981093bad51a7703039b4d32
hemashree91 <shreeraju91@gmail.com>
2024-03-29 17:52:20 +0800

    updated

---
+++ obje.js
@@ -0,0 +1,13 @@
+let obj1 = { name: "person1", age: 5 };
+let obj2 = { age: 5, name: "person1" };
+
+let str1 = JSON.stringify(obj1, Object.keys(obj1));
+console.log(str1);
+let str2 = JSON.stringify(obj2, Object.keys(obj2));
+console.log(str2);
+
+if (str1 !== str2) {
+  console.log("The JSON objects are equal");
+} else {
+  console.log("The JSON objects are not equal");
+}
